97e2eaf51e2e97b9d5da11c8e79275b0d0c79bfc
AmanVerma5 <amanv4775@gmail.com>
2023-11-10 01:21:05 +0530

    submitting code-Fri Nov 10 2023 01:21:02 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,50 @@
 
-import React from "react";
+import React, { useEffect,useState } from "react";
 import './../styles/App.css';
+import axios from "axios";
 
 const App = () => {
+  const [loading,setLoading]=useState(true);
+  const [data,setData]=useState(null);
+
+
+  useEffect(()=>{
+    axios.get('https://dummyjson.com/products')
+    .then(function(response){
+      console.log(response)
+      setData(response);
+      setLoading(false); 
+    }).catch((e)=>{console.log(e)
+      setLoading(false);
+    })
+    
+    setTimeout(()=>console.log('Dispaly>>>>',display),5000) 
+  },[])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        {
+          loading?<div>Loading...</div>:<div>
+            <h1>Data fetched from API</h1>
+            <pre>{data.data.products.map((element)=>{
+              return(
+                <div>
+                  <p>"id":{element.id}</p>
+                  <p>"title":{element.title}</p>
+                  <p>"description":{element.description}</p>
+                  <p>"price":{element.price}</p>
+                  <p>"discountPercentage":{element.discountPercentage}</p>
+                  <p>"rating":{element.rating}</p>
+                  <p>"stock":{element.stock}</p>
+                  <p>"brand":{element.brand}</p>
+                  <p>"category":{element.category}</p>
+                  <p>"thumbnail":{element.thumbnail}</p>
+                  <p>"images":{element.images}</p>
+                </div>
+              )
+            })}</pre>
+          </div>
+        }
     </div>
   )
 }
